75ef6e2dc5f10d96805a1507c2d1cd91f78d2ee4
Cagri Erdem <cagrierdemo@gmail.com>
2023-10-31 16:04:54 +0100

    importing syntax fix

--- src/js/pRoll.js
+++ src/js/pRoll.js
@@ -1,8 +1,9 @@
 // Author: Çağrı Erdem, 2023
 // Description: Interactive "piano roll" visualization for 2groove web app.
 
-import { Midi } from '@tonejs/midi';
+// import { Midi } from '@tonejs/midi';
 import p5 from 'p5';
+import { Midi } from 'tone';
 import { getTappedRhythms } from './interface.js';
 import { getCurrentMidiForVisuals } from './midiBroadcast.js'; // import from broadcasting script instead of globalFetch.js
 

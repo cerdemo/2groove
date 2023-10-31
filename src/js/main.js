6c7d889967708eb4cc03ba78df27fce5c777971c
Cagri Erdem <cagrierdemo@gmail.com>
2023-10-31 15:58:02 +0100

    css importing test

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,15 @@
 // Author: Çağrı Erdem, 2023
 // Description: Main bundler script for 2groove web app.
 
-// import "./../styles/efxUI.css";
-import "./../styles/mainUI.css";
-// import "./../styles/midiIO.css";
-// import "./../styles/pRoll.css";
-// import "./../styles/style.css";
+// import "./../styles/mainUI.css";
 // ----------------------------
 // ----------------------------
 import "./drSampler.js";
 import "./efx.js";
-// import "./efxUI.js";
 import "./globalFetch.js";
 import "./interface.js";
-// import "./midInput.js";
-// import "./mainUI.js";
 import "./midiBroadcast.js";
 import "./pRoll.js";
-// import "./utils/input.knobs.js";
 import "./workers/workerAjax.js";
 import "./workers/workerQueue.js";
 // ----------------------------
